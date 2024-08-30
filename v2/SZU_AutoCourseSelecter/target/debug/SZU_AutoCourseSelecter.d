537dea299049b8d61a00252560b49a66b9fc4ce5
andy <andywang2309724277@gmail.com>
2024-08-30 22:47:39 +0800

    新增v2版本

---
+++ v2/SZU_AutoCourseSelecter/target/debug/SZU_AutoCourseSelecter.d
@@ -0,0 +1,1 @@
+D:\SZU_AutoCourseSelecter\v2\SZU_AutoCourseSelecter\target\debug\SZU_AutoCourseSelecter.exe: D:\SZU_AutoCourseSelecter\v2\SZU_AutoCourseSelecter\src\main.rs
---
+++ v2/SZU_AutoCourseSelecter/target/debug/deps/SZU_AutoCourseSelecter.d
@@ -0,0 +1,5 @@
+D:\SZU_AutoCourseSelecter\v2\SZU_AutoCourseSelecter\target\debug\deps\SZU_AutoCourseSelecter.exe: src/main.rs
+
+D:\SZU_AutoCourseSelecter\v2\SZU_AutoCourseSelecter\target\debug\deps\SZU_AutoCourseSelecter.d: src/main.rs
+
+src/main.rs:
