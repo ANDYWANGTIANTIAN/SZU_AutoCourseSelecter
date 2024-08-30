c511d2475084de1d96557f5f45652a86e399424a
andy <andywang2309724277@gmail.com>
2024-08-30 23:13:45 +0800

    iced helloworld

--- v2/SZU_AutoCourseSelecter/src/main.rs
+++ v2/SZU_AutoCourseSelecter/src/main.rs
@@ -1,44 +1,35 @@
-use iced::widget::{button, column, text, Column};
-#[derive(Default)]
-struct Counter {
-    value: i32,
-}
-#[derive(Debug, Clone, Copy)]
+use iced::{Element, Sandbox, Settings};
+use iced::widget::text;
+struct GroceryList {}
+#[derive(Debug, Clone)]
 pub enum Message {
     Increment,
     Decrement,
 }
-impl Counter {
-    pub fn view(&self) -> Column<Message> {
-        // We use a column: a simple vertical layout
-        column![
-            // The increment button. We tell it to produce an
-            // `Increment` message when pressed
-            button("+").on_press(Message::Increment),
-
-            // We show the value of the counter here
-            text(self.value).size(50),
-
-            // The decrement button. We tell it to produce a
-            // `Decrement` message when pressed
-            button("-").on_press(Message::Decrement),
-        ]
-    }
-}
-impl Counter {
-    // ...
-
-    pub fn update(&mut self, message: Message) {
-        match message {
-            Message::Increment => {
-                self.value += 1;
-            }
-            Message::Decrement => {
-                self.value -= 1;
-            }
-        }
-    }
+impl Sandbox for GroceryList {
+	type Message = Message;
+	
+	/* Initialize your app */
+	fn new() -> GroceryList {
+		Self {}
+	}
+	
+	/**
+	* The title of the window. It will show up on the top of your application window.
+	*/
+	fn title(&self) -> String {
+		String::from("Grocery List App")
+	}
+	
+	fn update(&mut self, _message: Self::Message) {
+		// Update the state of your app
+	}
+	
+	fn view(&self) -> Element<Self::Message> {
+		text("This is where you will show the view of your app")
+		.into()
+	}
 }
 fn main() -> iced::Result {
-    iced::run("A cool counter", Counter::update, Counter::view)
+	GroceryList::run(Settings::default())
 }
