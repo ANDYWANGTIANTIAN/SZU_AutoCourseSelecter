b53f1a6755422a695ea152b5addea71240c0fbf3
ANDYWANGTIANTIAN <53755609+ANDYWANGTIANTIAN@users.noreply.github.com>
2024-08-30 22:49:16 +0800

    Delete v2/SZU_AutoCourseSelecter/target directory

--- v2/SZU_AutoCourseSelecter/target/debug/SZU_AutoCourseSelecter.d
+++
@@ -1,1 +0,0 @@
-D:\SZU_AutoCourseSelecter\v2\SZU_AutoCourseSelecter\target\debug\SZU_AutoCourseSelecter.exe: D:\SZU_AutoCourseSelecter\v2\SZU_AutoCourseSelecter\src\main.rs
--- v2/SZU_AutoCourseSelecter/target/debug/deps/SZU_AutoCourseSelecter.d
+++
@@ -1,5 +0,0 @@
-D:\SZU_AutoCourseSelecter\v2\SZU_AutoCourseSelecter\target\debug\deps\SZU_AutoCourseSelecter.exe: src/main.rs
-
-D:\SZU_AutoCourseSelecter\v2\SZU_AutoCourseSelecter\target\debug\deps\SZU_AutoCourseSelecter.d: src/main.rs
-
-src/main.rs:
