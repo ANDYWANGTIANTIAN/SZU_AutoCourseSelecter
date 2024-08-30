bf1f0409547753d9de15fdb8bd6e9f3d75770c47
andy <andywang2309724277@gmail.com>
2024-08-30 23:03:52 +0800

    新增v2版本

--- v2/SZU_AutoCourseSelecter/src/main.rs
+++ v2/SZU_AutoCourseSelecter/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{button, column, text, Column};
+#[derive(Default)]
+struct Counter {
+    value: i32,
+}
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    Increment,
+    Decrement,
+}
+impl Counter {
+    pub fn view(&self) -> Column<Message> {
+        // We use a column: a simple vertical layout
+        column![
+            // The increment button. We tell it to produce an
+            // `Increment` message when pressed
+            button("+").on_press(Message::Increment),
+
+            // We show the value of the counter here
+            text(self.value).size(50),
+
+            // The decrement button. We tell it to produce a
+            // `Decrement` message when pressed
+            button("-").on_press(Message::Decrement),
+        ]
+    }
+}
+impl Counter {
+    // ...
+
+    pub fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
+        }
+    }
+}
+fn main() -> iced::Result {
+    iced::run("A cool counter", Counter::update, Counter::view)
 }
