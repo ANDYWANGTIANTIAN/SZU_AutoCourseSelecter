f5ae425a84a2c9b3148d02cbdbe4bd7003f93da9
andy <andywang2309724277@gmail.com>
2024-08-31 00:06:53 +0800

    add js encryptor

--- v2/SZU_AutoCourseSelecter/src/main.rs
+++ v2/SZU_AutoCourseSelecter/src/main.rs
@@ -1,35 +1,11 @@
-use iced::{Element, Sandbox, Settings};
-use iced::widget::text;
-struct GroceryList {}
-#[derive(Debug, Clone)]
-pub enum Message {
-    Increment,
-    Decrement,
-}
-impl Sandbox for GroceryList {
-	type Message = Message;
-	
-	/* Initialize your app */
-	fn new() -> GroceryList {
-		Self {}
-	}
-	
-	/**
-	* The title of the window. It will show up on the top of your application window.
-	*/
-	fn title(&self) -> String {
-		String::from("Grocery List App")
-	}
-	
-	fn update(&mut self, _message: Self::Message) {
-		// Update the state of your app
-	}
-	
-	fn view(&self) -> Element<Self::Message> {
-		text("This is where you will show the view of your app")
-		.into()
-	}
-}
-fn main() -> iced::Result {
-	GroceryList::run(Settings::default())
+mod encryptor;
+
+use encryptor::QuickJsEncryptor;
+
+fn main() {
+    let encryptor = QuickJsEncryptor::new();
+    
+    let encrypted_data = encryptor.encrypt("hello world!", "this", "password", "is");
+    
+    println!("Encrypted data: {}", encrypted_data);
 }
